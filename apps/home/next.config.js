01ad1efc21869547c371e93c6dfa70922243c295
Antonette Caldwell <acald.nebula@gmail.com>
2023-06-11 16:39:17 -0500

    update remotes

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -5,8 +5,10 @@ const remotes = (isServer) => {
   return {
     widget: `widget@http://localhost:3004/_next/static/${location}/remoteEntry.js`,
     docs: `docs@http://localhost:3003/_next/static/${location}/remoteEntry.js`,
-    shop: `shop@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
-    home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    // shop: `shop@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
+    shop: `shop@https://nextjs13-app-mfe-7veq.vercel.app/_next/static/${location}/remoteEntry.js`,
+    // home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    home: `home@https://nextjs13-app-mfe-so6z.vercel.app/_next/static/${location}/remoteEntry.js`,
     // checkout: `checkout@http://localhost:3000/_next/static/${location}/remoteEntry.js`
     checkout: `checkout@https://nextjs13-app-mfe.vercel.app/_next/static/${location}/remoteEntry.js`,
   }
